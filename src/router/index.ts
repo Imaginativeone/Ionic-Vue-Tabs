d0071aab39b5e3ba3384c8cd37adb244700388d0
Doug Franklin <doug@dougfranklin.com>
2021-01-04 15:58:14 -0500

    Updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,19 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Tabs from '../views/Tabs.vue'
 
+const tab1Route = '@/views/Tab1.vue';
+const tab2Route = '@/views/Tab2.vue';
+const tab3Route = '@/views/Tab3.vue';
+
+// var as = ["max","jack","sam"];  
+// var s = as.sort(func);  
+
+// function func(a, b) {  
+//   return 0.5 - Math.random();
+// }  
+
+// console.log(s);
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -17,6 +30,7 @@ const routes: Array<RouteRecordRaw> = [
       },
       {
         path: 'tab1',
+        // component: () => import('@/views/Tab1.vue')
         component: () => import('@/views/Tab1.vue')
       },
       {
--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -34,6 +34,6 @@ export default {
       square, 
       triangle,
     }
-  }
+  },
 }
 </script>
